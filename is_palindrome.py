7e4825d26f1cf9038b2f91d10eadb6af2731cc9b
Nick <mrfarenyuk1@gmail.com>
2020-08-28 01:05:52 +0300

    Правка is_palindrome.py , добавление новой версии is_palindrome1(short).py

--- is_palindrome.py
+++ is_palindrome.py
@@ -5,6 +5,8 @@ def is_palindrome(string):
     x = len(string)
     i = 0
     j = x - 1
+    new_i = i
+    new_j = j
     if x == 1:
         print(True)
     elif x == 2:
@@ -13,13 +15,15 @@ def is_palindrome(string):
         else:
             print(False)
     else:
-        while i != j:
-            if string[i] == string[j]:
-                i += 1
-                j -= 1
+        while new_i != new_j:
+            if string[new_i] == string[new_j]:
+                new_i += 1
+                new_j -= 1
+                if i == new_j and j == new_i:
+                    break
             else:
                 break
-        print(string[i] == string[j])
+        print(string[new_i] == string[new_j])
 
 string = input('Input the word palindrome to check: ')
 is_palindrome(string)
